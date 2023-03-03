b026a91b1970e39b1915b52df1516f7ae9be8d39
marloncz <m.c.schumacher@live.de>
2023-03-03 10:34:51 +0100

    refactor: adds pre-commit hooks and refactored script

--- scripts/1_2_mnist_callback.py
+++ scripts/1_2_mnist_callback.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Flatten, Dense
 from tensorflow import keras
+from tensorflow.keras.layers import Dense, Flatten
+
 
 class MyCallback(tf.keras.callbacks.Callback):
     # Define the correct function signature for on_epoch_end
     def on_epoch_end(self, epoch, logs={}):
-        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.99:
+        if logs.get("accuracy") is not None and logs.get("accuracy") > 0.99:
             print("\nReached 99% accuracy so cancelling training!")
 
             # Stop training once the above condition is met
@@ -18,26 +19,31 @@ def train_mnist(x_train, y_train):
     callbacks = MyCallback()
 
     # Model
-    model = tf.keras.models.Sequential([
-        Flatten(input_shape=(28, 28)),
-        Dense(512, activation="relu"),
-        Dense(256, activation="relu"),
-        Dense(10, activation="softmax"),
-    ])
+    model = tf.keras.models.Sequential(
+        [
+            Flatten(input_shape=(28, 28)),
+            Dense(512, activation="relu"),
+            Dense(256, activation="relu"),
+            Dense(10, activation="softmax"),
+        ]
+    )
 
     # print summary
     model.summary()
 
     # Compile the model
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
 
     # Fit the model for 10 epochs adding the callbacks
     # and save the training history
     history = model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
 
     return history
+
 
 if __name__ == "__main__":
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
@@ -46,6 +52,9 @@ if __name__ == "__main__":
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
     data_shape = x_train.shape
-    print(f"There are {data_shape[0]} examples with shape ({data_shape[1]}, {data_shape[2]})")
+    print(
+        f"There are {data_shape[0]} examples with shape"
+        + f"({data_shape[1]}, {data_shape[2]})"
+    )
 
     hist = train_mnist(x_train, y_train)
